7e7e48df406d188909ad4109c5e6648e82555225
joehecn <leanbrown@live.cn>
2023-10-04 13:29:46 +0800

    home

--- lib/src/main.ts
+++ lib/src/main.ts
@@ -1,24 +1,14 @@
 import r2wc from "@r2wc/react-to-web-component";
 
-// import { configureStore } from 'app/store/configureStore';
-
 import { GDashboardGrid } from './g-dashboard-grid';
 import { GWelcome } from "./g-welcome";
 
-const getWC = () => {
-  // configureStore();
-
-  const GWelcomeWC = r2wc(GWelcome);
-  const GDashboardGridWC = r2wc(GDashboardGrid);
-
-  return {
-    GWelcomeWC,
-    GDashboardGridWC
-  };
-}
+const GWelcomeWC = r2wc(GWelcome);
+const GDashboardGridWC = r2wc(GDashboardGrid);
 
 export default {
   GWelcome,
   GDashboardGrid,
-  getWC
+  GWelcomeWC,
+  GDashboardGridWC
 };
