8ea2f7e065b5ad47e6c2eb4b587e5f6d2628fec6
joehecn <leanbrown@live.cn>
2024-03-17 12:33:22 +0800

    8.1.4-j13

--- public/views/joe-preload.js
+++ public/views/joe-preload.js
@@ -1,12 +1,4 @@
 window.joePreload = async function () {
-  // 删除 cookie
-  function deleteCookie(cname) {
-    const date = new Date();
-    date.setTime(date.getTime() - 10000);
-
-    document.cookie = cname + '=; expire=' + date.toGMTString() + '; path=/';
-  }
-
   function addCss(strCss) {
     //Copyright @ rainic.com
     try {
@@ -24,27 +16,6 @@ window.joePreload = async function () {
 
   if (window.location.pathname !== '/login') {
     // 已经到最终页面
-    // https://grafana-sandbox.cloud-building.com/public/views/gotoG.html?username=masikAdmin@mega.com&password=123456&path=/d/7Y9Yt0nIz/111&orgId=3
-    // https://grafana-sandbox.cloud-building.com/d/7Y9Yt0nIz/111?m=i&theme=light&orgId=3&refresh=25s
-
-    if (window.document.body.innerText.includes('Not Found')) {
-      // 404 页面
-      console.error('---- 警告: 不应该进入 404 页面 ----');
-
-      // 是否已经刷新过
-      const refreshed = localStorage.getItem('j-refreshed');
-      if (!refreshed) {
-        // 设置已经刷新过一次，防止死循环
-        localStorage.setItem('j-refreshed', '1');
-        // 删除 cookie
-        deleteCookie();
-        // 重新刷新页面
-        window.location.reload();
-
-        return;
-      }
-    }
-
     const type = localStorage.getItem('j-type');
 
     if (type === 'view') {
@@ -79,10 +50,6 @@ window.joePreload = async function () {
         .page-toolbar button[aria-label='Cycle view mode']  {display: none; }
         .page-toolbar button[title='Open dashboard settings']  {display: none; }
       `);
-      // addCss(`.css-14mr6ll>.css-1vzus6i-Icon  {display: none; }`)
-      // addCss(`.css-vyoujf  {display: none; }`)
-      // addCss(`.css-umstnt  {display: none; }`)
-      // addCss(`button[aria-label='Cycle view mode']  {display: none; }`)
     }
 
     document.getElementById('exLoding').classList.add('exLoding-hide');
@@ -92,7 +59,6 @@ window.joePreload = async function () {
     localStorage.removeItem('j-pass');
     localStorage.removeItem('j-type');
     localStorage.removeItem('j-rect');
-    localStorage.removeItem('j-refreshed');
 
     return;
   }
